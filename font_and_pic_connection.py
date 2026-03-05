88240d58a43b2d9ab936ab4a612f2afb4e4c5309
Harpiendro <wilsonharper07@gmail.com>
2026-03-04 21:46:57 -0500

    Change font and image paths to empty strings
    Updated font and image loading paths to empty strings.

--- font_and_pic_connection.py
+++ font_and_pic_connection.py
@@ -6,7 +6,7 @@ import constants
 pygame.font.init()
 
 try:
-	font_path = "C:/Users/wilso/Desktop/All.py/Arcade Central/press_start_2p/PressStart2P-Regular.ttf"
+	font_path = ""
 	mode_select_font = pygame.font.Font(font_path, 30)
 	game_title_font = pygame.font.Font(font_path, 45)
 	game_over_font = pygame.font.Font(font_path, 90)
@@ -17,9 +17,10 @@ except:
 	game_over_font = pygame.font.Font(None, 150)
 
 try:
-	trophy = pygame.image.load("C:/Users/wilso/Desktop/All.py/Arcade Central/Bricks/trophy.webp").convert_alpha()
+	trophy = pygame.image.load("").convert_alpha()
 	trophy_rect = trophy.get_rect()
 
 except pygame.error as e:
 	print(f"{constants.TERMINAL_RED}Error loading image: {e}{constants.TERMINAL_RESET}")
+
 	sys.exit()
