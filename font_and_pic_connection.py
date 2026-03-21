cab321b94c930df59fc060495c348eb7c5d59cee
Harpiendro <wilsonharper07@gmail.com>
2026-03-21 12:33:11 -0400

    Add comments for font and image path placeholders

--- font_and_pic_connection.py
+++ font_and_pic_connection.py
@@ -6,7 +6,7 @@ import constants
 pygame.font.init()
 
 try:
-	font_path = ""
+	font_path = "" # Must replace with your own path
 	mode_select_font = pygame.font.Font(font_path, 30)
 	game_title_font = pygame.font.Font(font_path, 45)
 	game_over_font = pygame.font.Font(font_path, 90)
@@ -17,7 +17,7 @@ except:
 	game_over_font = pygame.font.Font(None, 150)
 
 try:
-	trophy_raw = pygame.image.load("").convert_alpha()
+	trophy_raw = pygame.image.load("").convert_alpha() #<- Must replace with your own path
 	trophy = pygame.transform.scale(trophy_raw, (400, 400))
 	trophy_rect = trophy.get_rect()
 
